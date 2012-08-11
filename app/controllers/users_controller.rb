606f3fbb0ec85a8f4de91a0ca3d811dafa8fb17b
anhntk54 <anhntk54@gmail.com>
2012-08-11 14:38:06 +0700

    fix users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,12 +32,12 @@ class UsersController < ApplicationController
     end
   end
 
-   def show
+  def show
      @user = User.find(params[:id])
      respond_to do |format|
-          format.html {redirect_to @user}
-          format.json { render json: @user }
-        end
+        format.html 
+        format.json { render json: @user }
+     end
   end
 
   def edit
