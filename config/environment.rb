922abf1a2364612fa6cad848c5eac2d1752ab011
anhntk54 <anhntk54@gmail.com>
2012-08-30 11:37:16 +0700

    like query

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Project::Application.initialize!
+  DATABASE_OPERATOR = {
+    :like_operator => "LIKE"
+  }
