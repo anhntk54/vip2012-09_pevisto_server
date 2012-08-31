df25fe6ab978a1a2dd61af0d2551feaa7e6c6440
anhntk54 <anhntk54@gmail.com>
2012-08-31 14:27:27 +0700

    change search

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -89,7 +89,7 @@ class ApiController < ApplicationController
     respond_to do |format|
       if params[:id].to_i == 1
         like ='%'+ params[:s].concat("%")
-        product = Product.where("name ILKE ? ",like)
+        product = Product.where("name ILIKE ? ",like)
         format.json{render json: product}
       else
         like = params[:s].concat("%")
